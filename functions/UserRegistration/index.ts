39f5a371dfbcbc643d0ced597eb2f97892b9b9c0
venki <vensurag@gmail.com>
2021-12-21 22:34:44 +0000

    User Registration deployed changes

--- functions/UserRegistration/index.ts
+++ functions/UserRegistration/index.ts
@@ -8,6 +8,12 @@ exports.UserRegistrationHanlder = async(event:any,context:any,callback:any) =>{
 
    return {
        statusCode:200,
+       headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH",
+        "Access-Control-Allow-Origin" : "http://localhost:3000", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+    },
        body:JSON.stringify(data)
    }
 
