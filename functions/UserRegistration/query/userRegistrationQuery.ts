d78e4fcb57337c5ece8189b54215ca76bf856897
venki <vensurag@gmail.com>
2021-12-21 14:41:41 +0000

    The query changes fixes

--- functions/UserRegistration/query/userRegistrationQuery.ts
+++ functions/UserRegistration/query/userRegistrationQuery.ts
@@ -1,9 +1,12 @@
 export const UserRegistrationQuery = (event:any) => {
 console.log("watch",event)
 console.log("watch1",event.cognito_userid)
-    return `INSERT INTO user_registration ( cognito_userid,first_name,last_name,email, phone,date_of_birth ) 
-    VALUES (${event.cognito_userid.toString()},${event.first_name.toString()},${event.last_name.toString()},${event.email.toString()},${event.phone},${event.date_of_birth.toString()})
-   `
+console.log(`INSERT INTO user_registration (cognito_userid,first_name,last_name,email, phone,date_of_birth ) 
+VALUES ('${event.cognito_userid}','${event.first_name.toString()}','${event.last_name.toString()}','${event.email.toString()}','${event.phone}','${event.date_of_birth.toString()}')
+`)
+    return `INSERT INTO user_registration (cognito_userid,first_name,last_name,email, phone,date_of_birth ) 
+    VALUES ('${event.cognito_userid}','${event.first_name.toString()}','${event.last_name.toString()}','${event.email.toString()}','${event.phone}','${event.date_of_birth.toString()}')
+    `
 
 }
 
