cc39b812ee011b0dae1131b8a1bee070781fd14f
kiran_fr <kiranreddy8244@gmail.com>
2021-12-21 18:45:06 +0530

    user registration

--- functions/UserRegistration/query/userRegistrationQuery.ts
+++ functions/UserRegistration/query/userRegistrationQuery.ts
@@ -2,7 +2,7 @@ export const UserRegistrationQuery = (event:any) => {
 console.log("watch",event)
 console.log("watch1",event.cognito_userid)
     return `INSERT INTO user_registration ( cognito_userid,first_name,last_name,email, phone,date_of_birth ) 
-    VALUES('cognito_userid','first_name','last_name','email', '7075478244','2013-08-25')
+    VALUES (${event.cognito_userid.toString()},${event.first_name.toString()},${event.last_name.toString()},${event.email.toString()},${event.phone},${event.date_of_birth.toString()})
    `
 
 }
@@ -17,3 +17,5 @@ export const getUserRegistrationQuery = () =>{
 
 
  // VALUES (${event.cognito_userid},${event.first_name},${event.last_name},${event.email},${event.phone},${event.date_of_birth})
+ 
+//  VALUES('cognito_userid','first_name','last_name','email', '70754782454','2013-08-25')
