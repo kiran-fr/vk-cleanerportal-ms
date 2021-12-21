2c29136ae764413a3721951d16a652766cfbba20
kiran_fr <kiranreddy8244@gmail.com>
2021-12-21 18:18:58 +0530

    COMMITED

--- functions/UserRegistration/index.ts
+++ functions/UserRegistration/index.ts
@@ -3,7 +3,7 @@ import { Event } from "aws-sdk/clients/iotwireless";
 import { UserRegistrationServices } from "./services/UserRegistrationServices";
 
 exports.UserRegistrationHanlder = async(event:any,context:any,callback:any) =>{
-    console.log(event)
+    console.log("step1",event)
     const data = await UserRegistrationServices(event)
 
    return {
--- functions/UserRegistration/query/userRegistrationQuery.ts
+++ functions/UserRegistration/query/userRegistrationQuery.ts
@@ -1,9 +1,19 @@
 export const UserRegistrationQuery = (event:any) => {
-    return `INSERT INTO user_registration ( cognito_userid,first_name,last_name,email, phone,date_of_birth ) VALUES (${event.cognito_userid},${event.first_name},${event.last_name},${event.email},${event.phone},${event.date_of_birth})`
+console.log("watch",event)
+console.log("watch1",event.cognito_userid)
+    return `INSERT INTO user_registration ( cognito_userid,first_name,last_name,email, phone,date_of_birth ) 
+    VALUES('cognito_userid','first_name','last_name','email', '7075478244','2013-08-25')
+   `
+
 }
+
 
 export const getUserRegistrationQuery = () =>{
     return `
         SELECT * FROM user_registration
     `
 }
+
+
+
+ // VALUES (${event.cognito_userid},${event.first_name},${event.last_name},${event.email},${event.phone},${event.date_of_birth})
