b02386a2895923a736498ef6e87e305998c3dc1f
kiran_fr <kiranreddy8244@gmail.com>
2021-12-23 17:04:19 +0530

    user registration

--- functions/UserRegistration/index.ts
+++ functions/UserRegistration/index.ts
@@ -6,18 +6,8 @@ exports.UserRegistrationHanlder = async(event:any,context:any,callback:any) =>{
     // console.log('step 0', event.request.userAttributes)
     console.warn("step1"+ event.request.userAttributes)
     const data = await UserRegistrationServices(event.request.userAttributes)
-
-   return {
-       statusCode:200,
-       headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH",
-        "Access-Control-Allow-Origin" : "http://localhost:3000", // Required for CORS support to work
-        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
-    },
-       body:JSON.stringify(data)
+    
+    context.succeed(event)
    }
-
-}
 
 
