452eaef46564f095706b8393f96e45a347cf5c08
kiran_fr <kiranreddy8244@gmail.com>
2021-12-23 16:55:54 +0530

    user Email confirmation message(sns)

---
+++ functions/UserRegistration/helpers/EmailConfirmation.ts
@@ -0,0 +1,22 @@
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "us-east-1" });
+
+export const EmailConfirmation = (toAddress:any) =>{
+    
+        const params = {
+            Destination: {
+              ToAddresses: [toAddress],
+            },
+            Message: {
+              Body: {
+                Text: { Data: "Test" },
+              },
+        
+              Subject: { Data: "Test Email" },
+            },
+            Source: "venki.suragani@vrishkar.com",
+          };
+         
+          return ses.sendEmail(params).promise()
+    
+}
