65612f80a33bcdc9d9b41e606206d2fa497f7d02
venki <vensurag@gmail.com>
2021-12-23 18:36:02 +0000

    code refactor changes

--- functions/UserRegistration/services/UserRegistrationServices.ts
+++ functions/UserRegistration/services/UserRegistrationServices.ts
@@ -2,7 +2,7 @@ import { QueryTypes } from "sequelize/dist";
 import { connectDB, sequelizeConnection } from "../helpers/DbConnectionHelpers"
 import { getUserRegistrationQuery, UserRegistrationQuery } from "../query/userRegistrationQuery";
 
-export const UserRegistrationServices = async (userAttributes:any) => {
+export const UserRegistrationServices = (userAttributes:any) => {
     connectDB();
     return sequelizeConnection.query(UserRegistrationQuery(userAttributes), {
         type: QueryTypes.INSERT
