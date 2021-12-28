7166ac16b85d403fb5db483a78ed437141de26dc
kiran_fr <kiranreddy8244@gmail.com>
2021-12-28 14:25:14 +0530

    user registration service

--- functions/UserRegistration/index.ts
+++ functions/UserRegistration/index.ts
@@ -1,24 +1,6 @@
 import { UserRegistrationServices } from "./services/UserRegistrationServices";
 import { EmailConfirmation } from "./helpers/EmailConfirmationHelper/EmailConfirmation"
 
-<<<<<<< HEAD
-exports.UserRegistrationHanlder = async (event: any, context: any) => {
-    try {
-        console.warn("step1" + event.request.userAttributes)
-        console.warn("This is event", JSON.stringify(event.request.userAttributes.email))
-
-        // INSERT INTO REGISGRATION VALUES INTO DATABASE
-        const data = await UserRegistrationServices(event.request.userAttributes)
-
-        // Email Confirmation 
-        await EmailConfirmation(event.request.userAttributes.email)
-       
-        context.succeed(event)
-    } catch (exception) {
-        console.log(`This is UserRegistrationHanlder Error ${exception}`)
-    }
-}
-=======
 
 exports.UserRegistrationHanlder = async(event:any,context:any) =>{
     try {
@@ -32,6 +14,5 @@ exports.UserRegistrationHanlder = async(event:any,context:any) =>{
         console.log('Error is at UserRegistrationHanlder' + JSON.stringify(exception));
     }
    }
->>>>>>> f24ddac9aa649b38aadf37871eab50c72c03cacf
 
 
