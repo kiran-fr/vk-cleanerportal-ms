347ec705a73aac7c4d319fc739b4bc4a7db8e12d
kiran_fr <kiranreddy8244@gmail.com>
2021-12-28 14:10:49 +0530

    User registration service

--- functions/UserRegistration/services/UserRegistrationServices.ts
+++ functions/UserRegistration/services/UserRegistrationServices.ts
@@ -3,17 +3,16 @@ import { connectDB, sequelizeConnection } from "../helpers/DbConnectionHelpers"
 import { getUserRegistrationQuery, UserRegistrationQuery } from "../query/userRegistrationQuery";
 
 export const UserRegistrationServices = async (userAttributes:any) => {
-    connectDB();
-    // return sequelizeConnection.query('SELECT * FROM users' , {
-    //     type:QueryTypes.SELECT
-    // })
+    try {
+        connectDB();
+        await sequelizeConnection.query(UserRegistrationQuery(userAttributes), {
+            type: QueryTypes.INSERT
+        })
+    } catch (exception) {
+        console.log(`User Registration service Error ${exception}`)
+    }
+    
 
-    // return sequelizeConnection.query("INSERT INTO users (user_id, username,password ) VALUES(3, 'krishna','kiran@11')",{
-    //     type:QueryTypes.INSERT
-    // })
-
-    return sequelizeConnection.query(UserRegistrationQuery(userAttributes), {
-        type: QueryTypes.INSERT
-    })
+    
 }
 
