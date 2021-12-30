9e989e36dd21d6edad0381a4d85c6ee3b992f2e7
kiran_fr <kiranreddy8244@gmail.com>
2021-12-30 18:22:19 +0530

    terms and condtion data post method

--- functions/WorksManTermsAndCondition/query/WorksManTermsAndConditionQuery.ts
+++ functions/WorksManTermsAndCondition/query/WorksManTermsAndConditionQuery.ts
@@ -3,7 +3,7 @@ export const WorksManTermsAndConditionQuery = (event:any) => {
     console.log('this is event',event)
     return `
         INSERT INTO worksman_terms_and_conditions_table (worksman_id,isWatched_howVrishkar_Works,isUnderstand_Worksman_Will_SelfEmployeed,isUnderstand_Vrishkar_isnot_responsible)
-        VALUES ('${event.worksman_id}',${event.isWatched_howVrishkar_Works},${event.isUnderstand_Worksman_Will_SelfEmployeed},'${event.isUnderstand_Vrishkar_isnot_responsible}')
+        VALUES ('${event.worksman_id}','${event.isWatched_howVrishkar_Works}','${event.isUnderstand_Worksman_Will_SelfEmployeed}','${event.isUnderstand_Vrishkar_isnot_responsible}')
     `
 }
 
