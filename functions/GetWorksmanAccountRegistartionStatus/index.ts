e12dba558c591e2e464e0a39b786a094a2a29571
kiran_fr <kiranreddy8244@gmail.com>
2022-02-07 17:19:18 +0530

    merge conflicts resolved

--- functions/GetWorksmanAccountRegistartionStatus/index.ts
+++ functions/GetWorksmanAccountRegistartionStatus/index.ts
@@ -4,11 +4,7 @@ exports.GetWorksmanAccountRegistartionStatusHandler  = async(event:any) => {
     try {
         console.warn("This status event",event)
         // console.log(event.headers.email)
-<<<<<<< HEAD
         const Worksman: any = await getWorksManAccountStatusService(event.email)
-=======
-        const Worksman: any = await getWorksManAccountStatusService(event.headers.email)
->>>>>>> 6f4c0ffdf4595db889e67a2f29141606ad223643
         
         console.warn("This is worksmand id",Worksman)
         const data:any = await GetWorksmanAccountRegistartionStatusService(Worksman[0].worksman_id)
--- lib/resources/ApiGateway/ResponseModel.ts
+++ lib/resources/ApiGateway/ResponseModel.ts
@@ -1,19 +1,12 @@
 import * as apigateway from "@aws-cdk/aws-apigateway"
 
-<<<<<<< HEAD
 const successResponseModel = (api:any,success:any) => {
   return api.addModel(success, {
     contentType: 'application/json',
     modelName: success,
-=======
-const successResponseModel = (api: any, modelName: any, title: any) => {
-  return api.addModel(modelName, {
-    contentType: 'application/json',
-    modelName,
->>>>>>> 6f4c0ffdf4595db889e67a2f29141606ad223643
     schema: {
       schema: apigateway.JsonSchemaVersion.DRAFT4,
-      title: title,
+      // title: title,
       type: apigateway.JsonSchemaType.OBJECT,
       properties: {
         state: { type: apigateway.JsonSchemaType.STRING },
@@ -23,20 +16,13 @@ const successResponseModel = (api: any, modelName: any, title: any) => {
   });
 }
 
-<<<<<<< HEAD
 const errorResponseModel = (api: any,error:any) => {
    return api.addModel(error, {
     contentType: 'application/json',
     modelName: error,
-=======
-const errorResponseModel = (api: any, modelName: any, title: any) => {
-  return api.addModel(modelName, {
-    contentType: 'application/json',
-    modelName,
->>>>>>> 6f4c0ffdf4595db889e67a2f29141606ad223643
     schema: {
       schema: apigateway.JsonSchemaVersion.DRAFT4,
-      title: title,
+      // title: title,
       type: apigateway.JsonSchemaType.OBJECT,
       properties: {
         state: { type: apigateway.JsonSchemaType.STRING },
@@ -46,7 +32,6 @@ const errorResponseModel = (api: any, modelName: any, title: any) => {
   });
 }
 
-<<<<<<< HEAD
 export const responseMethods = (api: any,success:any,error:any) => {
     return [
         {
@@ -139,38 +124,3 @@ export const postresponseMethods = (api: any,respModal:any,errorModal:any) => {
       }
     ]
 }
-=======
-export const responseMethods = (api: any, sucessModelName: any, sucessTitle: any, errorModelName: any, errorTittle: any) => {
-  return [
-    {
-      // Successful response from the integration
-      statusCode: '200',
-      // Define what parameters are allowed or not
-      responseParameters: {
-        'method.response.header.Content-Type': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Headers': true
-      },
-      // Validate the schema on the response
-      responseModels: {
-        'application/json': successResponseModel(api, sucessModelName, sucessTitle)
-      }
-    },
-    {
-      // Same thing for the error responses
-      statusCode: '400',
-      responseParameters: {
-        'method.response.header.Content-Type': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Headers': true
-      },
-      responseModels: {
-        'application/json': errorResponseModel(api, errorModelName, errorTittle)
-      }
-    }
-  ]
-}
-
->>>>>>> 6f4c0ffdf4595db889e67a2f29141606ad223643
