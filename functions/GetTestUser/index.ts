f1c7815097b37faa3672ce3105f05f6ebf7f91fb
venki <vensurag@gmail.com>
2022-02-12 12:09:50 +0000

    changes to test 1

--- functions/GetTestUser/index.ts
+++ functions/GetTestUser/index.ts
@@ -3,7 +3,7 @@ exports.handler = async (event: any) => {
     console.log('step 1' +  JSON.stringify(event))
     const response = {
         statusCode: 200,
-        body: JSON.stringify({"id":"1","name":"venki test"})
+        body: JSON.stringify({"id":"1","name":"venki test 1"})
     };
     return response;
 }catch (exception) {
