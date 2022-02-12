c22e2acf947c0a5422240694316f5222eb38ce52
kiran_fr <kiranreddy8244@gmail.com>
2022-02-12 18:30:15 +0530

    added worksman id in getworkmanschedulequery

--- functions/GetWorksmanSchedules/query/GetWorksmanSchedulesQuery.ts
+++ functions/GetWorksmanSchedules/query/GetWorksmanSchedulesQuery.ts
@@ -1,4 +1,4 @@
 export const GetWorksmanSchedulesQuery = (event:any) => {
     
-    return `SELECT schedule_day,schedule_time from worksman_schedule where worksman_id ='${event.worksman_id}' `
+    return `SELECT worksman_id,schedule_day,schedule_time from worksman_schedule where worksman_id ='${event.worksman_id}' `
 } 
